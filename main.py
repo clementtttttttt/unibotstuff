e9e7da00c3e45d6df51cdd20a519d48d5e42c5e8
clement <clementchpoon@gmail.com>
2026-03-21 09:24:21 +0000

    first

---
+++ main.py
@@ -0,0 +1,150 @@
+from gpiozero import Device,PWMLED
+from time import sleep
+from ultralytics import YOLO
+import cv2
+import numpy as np
+from pupil_apriltags import Detector
+
+
+model = YOLO("best_ncnn_model",task="detect")
+
+CONFIDENCE_LEVEL = 0.7
+
+
+    
+MOTLF = 21
+MOTLB = 20
+MOTRF = 16
+MOTRB = 12
+
+motlf = PWMLED(MOTLF)
+motlb = PWMLED(MOTLB)
+motrf = PWMLED(MOTRF)
+motrb = PWMLED(MOTRB)
+
+motlf.frequency = (3333);
+motlb.frequency = (3333);
+motrf.frequency = (3333);
+motrb.frequency = (3333);
+
+def mot_left_turn(damn):
+    if damn >= 0:
+        motlf.value = damn;
+        motlb.off();
+    else:
+        motlf.off();
+        motlb.value = -damn;
+
+def go_straight(speed):
+    mot_right_turn(speed)
+    mot_left_turn(speed)
+    
+# ~ def go_left(speed,leftness):
+    # ~ mot_right_turn(speed)
+    # ~ mot_left_turn(-speed)
+
+def go_left(speed):
+    mot_right_turn(speed)
+    mot_left_turn(-speed)
+
+def go_right():
+    mot_left_turn(-speed)
+    mot_right_turn(speed)
+
+def mot_right_turn(damn):
+        if damn > 0:
+                motrf.value = damn;
+                motrb.off();
+        else:
+                motrf.off();
+                motrb.value = -damn;
+
+def activate_collection_spinner():
+    return
+
+feature_params = dict(maxCorners=1,qualityLevel=.6,minDistance=25,blockSize=9)
+def should_i_quit():
+    return
+def camera_check():
+    #print("SHOW IMAGE!")
+    ret, img = cap.read();
+    #img2 = img.img_to_array(img, dtype='uint8')
+    #ball = cv2.goodFeaturesToTrack(img, **feature_params)
+    results = model(img, stream=True,verbose=False)
+    
+    y = False
+    
+    for result in results:
+        for box in result.boxes:
+            if box.conf[0] > CONFIDENCE_LEVEL:
+                y = True
+    
+    if y:
+        print("WOOOO", end="", flush=True)
+    cv2.imshow("Image", img);
+    cv2.waitKey(1)
+    return y;
+
+
+cap = cv2.VideoCapture(0);
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)  
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+cap.set(cv2.CAP_PROP_FPS, 60)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+def met_parking_criteria(colcnt):
+    if colcnt > 8 or True:
+        return True
+    return False
+
+def park_and_unload():
+    
+    end = False
+    while(not end):
+        ret, rawimg = cap.read()
+        img = cv2.cvtColor(rawimg, cv2.COLOR_BGR2GRAY)
+        at_detector = Detector(
+           families="tag36h11",
+           nthreads=4,
+           quad_decimate=1.0,
+           quad_sigma=0.0,
+           refine_edges=1,
+           decode_sharpening=0.25,
+           debug=0
+        )
+
+        tags  = at_detector.detect(img)
+        cv2.imshow("Image", img);
+        cv2.waitKey(1)
+        print("safs")
+        #print(tags)
+        if tags != null:
+            print(tags[0])
+
+activate_collection_spinner();
+state = "scan"
+
+counter = 0;
+
+while not should_i_quit():
+    found_balls = camera_check();
+    if met_parking_criteria(counter):
+        park_and_unload();
+        
+        counter = 0
+
+
+    if not found_balls:
+        print("Turning left...")
+        
+        go_straight(-1);
+        sleep(0.1)
+        go_left(1);
+        sleep(0.1)
+    else:
+        found_balls = camera_check(); #confirmation
+        print ("Straight on!")
+        go_straight(1);
+        sleep(2.5)
+        counter += 8;
+    
+
