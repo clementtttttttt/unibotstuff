4def8d60ecb56b69fc669c23624454646c15cb05
clement <clementchpoon@gmail.com>
2026-05-20 01:52:32 +0100

    update main.py with version from rpi

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import numpy as np
 from pupil_apriltags import Detector
 import time
 
+from smbus2 import SMBus
+
 
 START_TIME = None
 
@@ -18,7 +20,18 @@ CONFIDENCE_LEVEL = 0.7
 MATCH_LEN_SEC = 175
 ALIGN_AREA_W = 20
 
-IW = 427
+at_detector = Detector(
+        families="tag36h11",
+        nthreads=4,
+        quad_decimate=1.0,
+        quad_sigma=0.0,
+        refine_edges=1,
+        decode_sharpening=0.25,
+        debug=0
+)
+
+
+IW = 425
 IH = 240
     
 MOTRF = 20
@@ -80,9 +93,14 @@ def activate_collection_spinner():
     motspin.value = 0.2
     return
 
+def switch_is_pressed():
+    return True
+    return sw.is_pressed
+    
+
 feature_params = dict(maxCorners=1,qualityLevel=.6,minDistance=25,blockSize=9)
 def should_i_quit():
-    if not sw.is_pressed:
+    if not switch_is_pressed():
         return True
     if how_long_till_end() < 0: 
         return True
@@ -117,26 +135,113 @@ cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
 
 
+bus = SMBus(1)
+
+def to_signed16(n):
+    n = n & 0xffff
+    return n | (-(n & 0x8000))
+
+
+
+def compass_wait_data():
+    while (bus.read_byte_data(0x2c, 0x09) & 1) == 0:
+        sleep(0) 
+
+def init_compass():
+    bus.write_byte_data(0x2c, 0xb, 0x80)
+    sleep(0.1)
+    
+    bus.write_byte_data(0x2c, 0x29, 0x6)
+    bus.write_byte_data(0x2c, 0xb, 0x00)    
+    bus.write_byte_data(0x2c, 0xa, 0xc3)
+
+    compass_wait_data()
+
+    fa = bus.read_word_data(0x2c, 0x1)
+    fb = bus.read_word_data(0x2c, 0x3)
+    fc = bus.read_word_data(0x2c, 0x5)
+    
+    bus.write_byte_data(0x2c, 0xb, 0x40)
+    
+    sleep(0.2)
+    
+    compass_wait_data()
+
+    sa = bus.read_word_data(0x2c, 0x1)
+    sb = bus.read_word_data(0x2c, 0x3)
+    sc = bus.read_word_data(0x2c, 0x5)
+    
+    global dx, dy, dz
+    dx = to_signed16(fa) - to_signed16(sa)
+    dy = to_signed16(sb) - to_signed16(fb)
+    dz = to_signed16(sc) - to_signed16(fc)
+
+
+
+    while (bus.read_byte_data(0x2c, 0xb) & 0x40) != 0:
+        sleep(0) 
+    bus.write_byte_data(0x2c, 0x29, 0x6)
+    bus.write_byte_data(0x2c, 0xb, 0x00)
+    bus.write_byte_data(0x2c, 0xa, 0xcd)    
+
 
 def met_parking_criteria(colcnt):
-    if colcnt > 8 or True:
+    if colcnt > 8:
         return True
     return False
+
+def compass_get_range():
+    rngbit= (bus.read_byte_data(0x2c,0xb) >> 2)  & 0b11
+    match(rngbit):
+        case 0:
+            return 1000;
+        case 1:
+            return 2500;
+        case 2:
+            return 3750;
+        case 3:
+            return 15000;
+    
+from math import atan2, pi
+
+_ctx = None
+_cty = None
+_cbx = None
+_cby = None
+
+def _update_cal(x, y):
+    global _ctx, _cty, _cbx, _cby
+    if _ctx is None:
+        _ctx = _cbx = x
+        _cty = _cby = y
+    else:
+        if x < _ctx: _ctx = x
+        if x > _cbx: _cbx = x
+        if y < _cty: _cty = y
+        if y > _cby: _cby = y
+
+def get_heading():
+    compass_wait_data()
+    raw_x = to_signed16(bus.read_word_data(0x2c, 0x1))
+    raw_y = to_signed16(bus.read_word_data(0x2c, 0x3))
+
+    _update_cal(raw_x, raw_y)
+    cal_x = raw_x - (_ctx + _cbx) / 2.0
+    cal_y = raw_y - (_cty + _cby) / 2.0
+
+    heading = atan2(cal_y, cal_x) * 180.0 / pi
+    if heading < 0:
+        heading += 360.0
+    return heading
 
 
 def get_targ_tag():
     ret, rawimg = cap.read()
     img = cv2.cvtColor(rawimg, cv2.COLOR_BGR2GRAY)
-    at_detector = Detector(
-        families="tag36h11",
-        nthreads=4,
-        quad_decimate=1.0,
-        quad_sigma=0.0,
-        refine_edges=1,
-        decode_sharpening=0.25,
-        debug=0
-    )
+
+    
     tags  = at_detector.detect(img)
+    
     cv2.imshow("Image", img);
     cv2.waitKey(1)
 
@@ -150,6 +255,20 @@ def get_targ_tag():
     return ret
 
 def backwards_and_180():
+    
+    go_straight(-0.4)
+    sleep(0.4)
+    go_straight(0);
+    
+    curr_heading = get_heading();
+    
+    targ_heading = (curr_heading + 180) % 360;
+    
+    while(get_heading() < (targ_heading - TARG_HEADING_TOLERANCE) or get_heading() > (targ_heading + TARG_HEADING_TOLERANCE)):
+        go_left(0.5)
+        sleep(0.1)
+        go_left(0)
+        sleep(0.1)
     return
     
 def activate_unload():
@@ -176,8 +295,7 @@ def align_to_tag():
             else:
                 aligned = True
                 
-    backwards_and_180()
-    activate_unload()
+
     
 
 def park_and_unload():
@@ -188,18 +306,21 @@ def park_and_unload():
         wait(2)
         go_straight(-0.5)
         wait(0.5)
+    backwards_and_180()
+    activate_unload()
 
 
 def wait_start():
     #gpio detection logic goes here
     start = False
     while not start:
-        if sw.is_pressed:
+        if switch_is_pressed:
             start = True 
 activate_collection_spinner();
 state = "scan"
 
 counter = 0;
+init_compass();
 
 camera_check()
 wait_start()
@@ -209,11 +330,14 @@ START_TIME = time.time()
 
 
 
+
+
 while not should_i_quit():
+    print(get_heading());
+
     found_balls = camera_check();
     if met_parking_criteria(counter):
-        park_and_unload();
-        
+        park_and_unload();  
         counter = 0
 
 
